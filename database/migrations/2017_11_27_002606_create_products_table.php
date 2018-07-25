04b86c7f23c8d5af40f29775bab9620686d76fca
Rodolfo Kuman <rodolfo.kuman@hotmail.com>
2018-07-24 19:29:19 -0500

    migraciones

--- database/migrations/2017_11_27_002606_create_products_table.php
+++ database/migrations/2017_11_27_002606_create_products_table.php
@@ -15,6 +15,13 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('description', 1000);
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::PRODUCTO_NO_DISPONIBLE);
+            $table->string('image');
+            $table->integer('seller_id')->unsigned();
+            $table->foreign('seller_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
