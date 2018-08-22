db0cc26e751531c732c0ad684bd6001852ecf5e3
Rodolfo Kuman <rodolfo.kuman@hotmail.com>
2018-08-21 22:21:54 -0500

    Configuracion mails

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,11 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\UserCreated;
 use App\Product;
+use App\User;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        User::created(function($user){
+          Mail::to($user)->send(new UserCreated($user));
+        });
 
         Product::updated(function($product){
           if($product->quantity == 0 && $product->estaDisponible()){
